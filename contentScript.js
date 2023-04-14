6618e0ed6620d286ae25f8bb6b811af6cc98119d
Lakshya Taragi <lakshya.taragi01@gmail.com>
2023-04-14 14:01:56 +0530

    Implement copying for single response

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,6 @@
 const CHAT = "CHAT";
 const CG_CLASS = "relative h-[30px] w-[30px] p-1 rounded-sm text-white flex items-center justify-center";
+const CODE = "Copy code";
 
 (() => {
     
@@ -13,11 +14,40 @@ const CG_CLASS = "relative h-[30px] w-[30px] p-1 rounded-sm text-white flex item
         }
     });
 
+    const waitForChats = () => {
+        return new Promise((resolve) => {
+            const check = () => {
+                const allResponses = document.getElementsByClassName(CG_CLASS);
+                if (allResponses.length > 0) {
+                    resolve(allResponses);
+                } else {
+                    setTimeout(check, 100);
+                }
+            };
+            check();
+        });
+    };
+
     const newChatLoaded = () => {
-        console.log("newChatLoaded@", currentChat);
+        waitForChats().then((allResponses) => {
+            let response = allResponses[allResponses.length - 1];
+            response.addEventListener("click", (e) => {
+                let contents = e.target.parentNode.parentNode.parentNode.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes;
+                let copyText = "";
+                contents.forEach(section => {
+                    if(section instanceof HTMLParagraphElement){
+                        copyText += section.textContent + "\n";
+                    } else if (section.textContent.includes(CODE)){
+                        copyText += section.textContent.split(CODE)[1] + "\n";
+                    }
+                });
+                navigator.clipboard.writeText(copyText)
+                .then(() => {console.log("text copied!");})
+                .catch((err) => {console.log("failed to copy", err)});
+            });
+        });
     };
 
 })();
-
 
 
