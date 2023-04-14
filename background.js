cf24c8bf57bf46f3b4309218414cdf9fc86d9fe5
Lakshya Taragi <lakshya.taragi01@gmail.com>
2023-04-15 00:19:50 +0530

    Add copyButton to new responses - fixes (#4)

--- background.js
+++ background.js
@@ -1,12 +1,13 @@
 const CHAT = "CHAT";
-const CHAT_REGEX = /chat.openai.com\/c(hat)?\/[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+$/;
+const NEW_RESPONSE = "NEW_RESPONSE";
+const CHAT_REGEX = /chat.openai.com\/c(hat)?\/[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+-[a-zA-Z0-9]+(\/)?$/;
+const CONVERSATION = "https://chat.openai.com/backend-api/conversation";
+const POST = "POST";
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === "complete") {
-        console.log("Tab loaded: ", tab.url);
         if (tab.url && CHAT_REGEX.test(tab.url)) {
             const chatId = tab.url.split("/").pop();
-            console.log(chatId);
             chrome.tabs.sendMessage(tabId, {
                 type: CHAT,
                 chatId
@@ -14,3 +15,11 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
         }
     }
 });
+
+chrome.webRequest.onCompleted.addListener((request) => {
+    if (request.url.includes(CONVERSATION) && request.method === POST) {
+        chrome.tabs.sendMessage(request.tabId, {
+            type: NEW_RESPONSE
+        });
+    }
+}, { urls: ["<all_urls>"] }, []);
--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,5 @@
 const CHAT = "CHAT";
+const NEW_RESPONSE = "NEW_RESPONSE";
 const CG_CLASS = "relative h-[30px] w-[30px] p-1 rounded-sm text-white flex items-center justify-center";
 const CODE = "Copy code";
 const OG_COLOR = "rgb(16, 163, 127)";
@@ -13,6 +14,10 @@ const COPY_BUTTON = "rgb(239, 92, 128)";
         if (type === CHAT) {
             currentChat = chatId;
             newChatLoaded();
+        } else if (type === NEW_RESPONSE) {
+            waitForChats().then((allResponses) => {
+                addCopyButton(allResponses[allResponses.length - 1]);
+            });
         }
     });
 
@@ -30,57 +35,58 @@ const COPY_BUTTON = "rgb(239, 92, 128)";
         });
     };
 
+    const addCopyButton  = (response) => {
+        let child = response.firstChild;
+        let grandChild = child.firstChild;
+
+        // copy-text
+        response.addEventListener("click", () => {
+            let contents = response.parentNode.parentNode.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes;
+            let copyText = "";
+
+            // copy each section
+            contents.forEach(section => {
+                if (section instanceof HTMLParagraphElement) {
+                    copyText += section.textContent + "\n";
+                } else if (section.childElementCount > 0) {
+                    section.childNodes.forEach(subsec => {
+                        copyText += subsec.textContent + "\n";
+                    });
+                } else if (section.textContent.includes(CODE)) {
+                    copyText += section.textContent.split(CODE)[1] + "\n";
+                }
+            });
+            navigator.clipboard.writeText(copyText)
+                .then(() => { console.log("text copied!"); })
+                .catch((err) => { console.log("failed to copy", err) });
+
+            // copied-blink
+            grandChild.setAttribute("fill", "black");
+            setTimeout(() => {
+                grandChild.setAttribute("fill", "currentColor");
+            }, 100);
+        });
+
+        // hover-effects
+        response.addEventListener("mouseover", () => {
+            response.style.backgroundColor = COPY_BUTTON;
+            child.style.backgroundColor = COPY_BUTTON;
+        });
+        child.addEventListener("mouseover", () => {
+            child.style.backgroundColor = COPY_BUTTON;
+            response.style.backgroundColor = COPY_BUTTON;
+        });
+        response.addEventListener("mouseout", () => {
+            response.style.backgroundColor = OG_COLOR;
+            child.style.backgroundColor = OG_COLOR;
+        });
+    };
+
     const newChatLoaded = () => {
         waitForChats().then((allResponses) => {
             for (let i = allResponses.length - 1; i >= 0; i--) {
-                let response = allResponses[i];
-                let child = response.firstChild;
-                let grandChild = child.firstChild;
-
-                // copy-text
-                response.addEventListener("click", () => {
-                    let contents = response.parentNode.parentNode.childNodes[1].childNodes[0].childNodes[0].childNodes[0].childNodes;
-                    let copyText = "";
-
-                    // copy each section
-                    contents.forEach(section => {
-                        if (section instanceof HTMLParagraphElement) {
-                            copyText += section.textContent + "\n";
-                        } else if (section.childElementCount > 0) {
-                            section.childNodes.forEach(subsec => {
-                                copyText += subsec.textContent + "\n";
-                            });
-                        } else if (section.textContent.includes(CODE)) {
-                            copyText += section.textContent.split(CODE)[1] + "\n";
-                        }
-                    });
-                    navigator.clipboard.writeText(copyText)
-                        .then(() => { console.log("text copied!"); })
-                        .catch((err) => { console.log("failed to copy", err) });
-
-                    // copied-blink
-                    grandChild.setAttribute("fill", "black");
-                    setTimeout(() => {
-                        grandChild.setAttribute("fill", "currentColor");
-                    }, 100);
-                });
-
-                // hover-effects
-                response.addEventListener("mouseover", () => {
-                    response.style.backgroundColor = COPY_BUTTON;
-                    child.style.backgroundColor = COPY_BUTTON;
-                });
-                child.addEventListener("mouseover", () => {
-                    child.style.backgroundColor = COPY_BUTTON;
-                    response.style.backgroundColor = COPY_BUTTON;
-                });
-                response.addEventListener("mouseout", () => {
-                    response.style.backgroundColor = OG_COLOR;
-                    child.style.backgroundColor = OG_COLOR;
-                });
-
+                addCopyButton(allResponses[i]);
             }
-
         });
     };
 
